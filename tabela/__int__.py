31c1b6798ebea705ae0f421f935b7ba0be453ac6
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-07-19 23:47:11 -0300

    3.4

--- tabela/__int__.py
+++ tabela/__int__.py
@@ -2,8 +2,10 @@ def area_circulo(radio):
     return 3.14 * radio * radio
 
 def perimetro_circulo(radio):
-    return 3.14 * 2 * radio
+    return 3.14 * 2 * radioo
 
 
 
 #Radio e raio em potugues ou seja sera mutiplicado pelo raio de 3.14
+
+#Inquerente atualizar esse codigo
