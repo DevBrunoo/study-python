f596b7d6eacd45dd453aa902651b6a388e81d050
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-11-01 23:10:03 -0300

    nov

--- apagar.py
+++ apagar.py
@@ -57,23 +57,23 @@ if __name__ == "__main__":
 
 
 
-''' 
-Na copa do mundo a fase eliminatoria e composta de 16 equipes, e cada roda uma equipe jogando contra a outra, as equipes que perdem ser excluidas ou seja eliminadas
-Depois da copa as equipes recebem classificacao da FIFA, as classificacoes mais altas significam mais numeros de viroias, com base nessas classificacoes que estao no arquivo ao lado e capaz de simular a proxima equipe vencedora
-Usando as informacoes do lado conseguimos simular todo o torneio, com isso saberemos os times que mais tem chamce de ganhar. 
 
-Esse aquivo python tem como tarefa simular, e tabelar os times com mais chance de ganhar
 
-Olhando o arquivo 2018m.csv voce vera 16 equipes e suas classificações 
-esse aquivo posui duas colunas, uma chamada team "com nome do pais" e outra chamada rating(representando a classificação da equipe)
-Vale lembrar que as equipes ja estao em ordem de enfrentamento no arquivo.
+#Na copa do mundo a fase eliminatoria e composta de 16 equipes, e cada roda uma equipe jogando contra a outra, as equipes que perdem ser excluidas ou seja eliminadas
+#Depois da copa as equipes recebem classificacao da FIFA, as classificacoes mais altas significam mais numeros de viroias, com base nessas classificacoes que estao no arquivo ao lado e capaz de simular a proxima equipe vencedora
+#Usando as informacoes do lado conseguimos simular todo o torneio, com isso saberemos os times que mais tem chamce de ganhar. 
 
-Podemos representar os times em python da seguinte maneira com nome da equipe e a sua classificação, nesse exemplo estamos representando o uruguai {"team": "Uruguay", "rating": 976}
-no arquivo 2019w.csv, contem os dados comentados dos times da copa do mundo da seleção feminina. 
+#Esse aquivo python tem como tarefa simular, e tabelar os times com mais chance de ganhar
 
-A variavel N descrita no codigo, representa o numero de simulações que nesse caso serão 1000.
-Simulate_game e a função que aceita dois times de entrada "lembrando que cada time e um dicionario contendo sua classificação e nome"
-A função simulate_round aceita uma lista de times em uma variavel chamada teans, e simula jogos contra cada par de time, entao a funcao retorna uma lista de todas as equipes que venceral a rodada
+#Olhando o arquivo 2018m.csv voce vera 16 equipes e suas classificações 
+#esse aquivo posui duas colunas, uma chamada team "com nome do pais" e outra chamada rating(representando a classificação da equipe)
+#Vale lembrar que as equipes ja estao em ordem de enfrentamento no arquivo.
+
+#Podemos representar os times em python da seguinte maneira com nome da equipe e a sua classificação, nesse exemplo estamos representando o uruguai {"team": "Uruguay", "rating": 976}
+#no arquivo 2019w.csv, contem os dados comentados dos times da copa do mundo da seleção feminina. 
+
+#A variavel N descrita no codigo, representa o numero de simulações que nesse caso serão 1000.
+#Simulate_game e a função que aceita dois times de entrada "lembrando que cada time e um dicionario contendo sua classificação e nome"
+#A função simulate_round aceita uma lista de times em uma variavel chamada teans, e simula jogos contra cada par de time, entao a funcao retorna uma lista de todas as equipes que venceral a rodada
 
  
- '''
---
+++ phonebook.py
@@ -0,0 +1,10 @@
+import csv
+from cs50 import get_string
+
+file = open("phonebook.csv", "a")
+
+name = get_string("Name: ")
+number = get_string("Number: ")
+
+writer = csv.writer(file)
+writer.writerow([name, number])
