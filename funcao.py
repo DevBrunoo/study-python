385a52eaeaf148f8bfcf4821fe548fc1f2b7f081
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-11-05 19:11:38 -0300

    2

--- funcao.py
+++ funcao.py
@@ -7,3 +7,14 @@ elif x > y:
     print("x is greater tha y")
 else:
     print("x is equal to y")
+    #Como escrever uma funcao em Python
+
+
+if x < y:
+    print("x is less tah y")
+elif x > y:
+    print("x is greater tha y")
+else:
+    print("x is equal to y")
+    
+    
