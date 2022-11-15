9e9b267c9b6445f59e5862dc33ad4ab0aae27d82
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-11-15 19:59:01 -0300

    lo

---
+++ favorites4.py
@@ -0,0 +1,14 @@
+import csv 
+
+counter = 0
+
+with open("favorites.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        title = row["title"].string().upper()
+        if re.search("OFFICE", title):
+            counter += 1
+            
+print(f"Number of people who like The Office: {counter}") 
+
+
