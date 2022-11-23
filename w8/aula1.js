7712a010a82137c268b76f52f0691bed6dc850c1
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-11-22 23:46:45 -0300

    dormes

---
+++ w8/aula1.js
@@ -0,0 +1,49 @@
+/* A sintaxe do JS e semelhante a de C e Python */
+
+let counter = 0;
+
+counter = counter + 1;
+counter += 1;
+counter++;
+
+if (x < y)
+{
+
+}
+
+if (x < y)
+{
+
+}
+else
+{
+
+}
+
+
+/* else */
+
+
+if (x < y)
+{
+
+}
+else if (x > y)
+{
+
+}
+else
+{
+
+}
+
+
+while (true)
+{
+
+}
+
+for (let i = 0; i < 3; i++)
+{
+
+}
